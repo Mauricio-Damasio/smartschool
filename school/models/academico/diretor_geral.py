f57b3d21bb85cb9e1353bf30d9e79c87fd5a2dd0
Maurício Damásio <mauriciodamasio123@gmail.com>
2025-07-16 11:03:58 +0100

    Adiciona migrações para o modelo Diretor

--- school/models/academico/diretor_geral.py
+++ school/models/academico/diretor_geral.py
@@ -5,12 +5,9 @@ from django.contrib.auth.models import User
 
 class Diretor(Pessoa):
  
-  
   numAgente = models.CharField(max_length=25, unique=True)
   user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
 
-  
-  
   class  Meta:
     
     db_table = 'Diretor'
